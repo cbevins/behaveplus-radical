2a226ca1ed8052627d05e9cdaa19217071357310
Collin D. Bevins <cbevins@montana.com>
2020-07-31 12:39:17 -0600

    Initialized clean room

--- src/__tests__/import.test.js
+++
@@ -1,4 +0,0 @@
-import Dag from 'behaveplus-models'
-test('1: basic', () => {
-  expect(1).toEqual(2)
-})
--- src/utils/import.core.js
+++
@@ -1,4 +0,0 @@
-import { BehavePlus } from 'behaveplus-core'
-
-const dag = new BehavePlus.BpxDag('core')
-console.log(`behaveplus-core Dag has ${dag.node.map.size} Nodes`)
